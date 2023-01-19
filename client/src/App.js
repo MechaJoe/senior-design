9f4d291361210535639e0f639bf0f4eb7a584f2a
Joe Konno <jo3konno@gmail.com>
2023-01-18 19:02:09 -0500

    Update App.js to use React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,26 @@
-import logo from './logo.svg'
+// import logo from './logo.svg'
+// import ReactDOM from 'react-dom/client'
 import './App.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
+import Login from './pages/Login'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: 'login',
+    element: <Login />,
+  },
+])
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   )
 }
 
