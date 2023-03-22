fabdd1ec2d03bafb7877f2f2e8b90d465704eef3
Joe Konno <jo3konno@gmail.com>
2023-03-21 20:39:25 -0400

    Fix groups tab bug

--- client/src/components/groups_components/JoinGroupButton.js
+++ client/src/components/groups_components/JoinGroupButton.js
@@ -7,7 +7,7 @@ export default function JoinGroupButton(props) {
     <button
       type="button"
       className="rounded-2xl w-32 h-32 bg-gray-100 border-dashed border border-gunmetal"
-      onClick={joinGroup(classCode, assignmentId, groupId)}
+      onClick={() => joinGroup(classCode, assignmentId, groupId)}
       onKeyDown={joinGroup}
     >
       +
